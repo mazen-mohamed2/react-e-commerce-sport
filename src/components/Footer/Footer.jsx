160211cbc951c37da46559a97c1e10ff878d1416
Mazen Mohamed <99109199+mazen-mohamed2@users.noreply.github.com>
2022-12-14 14:00:55 +0200

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,71 @@
+import {
+  Avatar,
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  // Spacer,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import React from "react";
+import portfolio from "../../img/portfolio.png";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+// import { GiCondorEmblem } from "react-icons/gi";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  const [isLargerThan] = useMediaQuery("(min-width: 768px)");
+  const [isSmallerThan] = useMediaQuery("(min-width: 468px)");
+
+  return (
+    <div className="Footer">
+      <Box
+        bg="black"
+        color="whitesmoke"
+        height={isSmallerThan ? "50vh" : "50vh"}
+        pt="3rem"
+        lineHeight="2rem"
+      >
+        <Flex
+          justify={"space-evenly"}
+          width={["100%", "100%", "100%", "100%"]}
+          textAlign={isSmallerThan ? "left" : "center"}
+          fontSize={["sm", "md", "md", "md"]}
+          flexDirection={isSmallerThan ? "row" : "column"}
+        >
+          <Box as={Flex} flexDirection="column">
+            <Heading>Product</Heading>
+            <Text as={Link} to="/allproducts?gender=MEN">Mens Collection</Text>
+            <Text as={Link} to="/allproducts?gender=WOMEN">Womens Collection</Text>
+            <Text as={Link} to="/allproducts?category=shoes" >Shoes Collection</Text>
+            <Text as={Link} to="/allproducts?category=clothes">Clothes Collection</Text>
+          </Box>
+
+          {isSmallerThan ? (
+            <Box>
+              <Heading>Support</Heading>
+              <Text>Help</Text>
+              <Text>Customer Service</Text>
+              <Text>Shipping</Text>
+              <Text>Order Tracker</Text>
+              <Text>Returns & Exchanges</Text>
+            </Box>
+          ) : null}
+
+          {isLargerThan ? (
+            <Box>
+              <Heading>Company Info</Heading>
+              <Text>About Us</Text>
+              <Text>Entity Details</Text>
+              <Text>Careers</Text>
+              <Text>Company Apps</Text>
+            </Box>
+          ) : null}
+        
+        </Flex>
+      </Box>
+    </div>
+  );
+};
+
+export default Footer;
